40f186a62bea8a8f6a7f6ab68b9623495e4b686f
Gal Gur-Arie <gal.ga75@gmail.com>
2014-07-22 00:15:19 +0300

    Added the JSON fetch request with the 5 seconds loop

--- red_alarm.php
+++ red_alarm.php
@@ -2,18 +2,28 @@
 set_time_limit(0);
 date_default_timezone_set('Asia/Jerusalem');
 
+
+function fetchJSON()
+{
+	$JSON = file_get_contents('http://www.oref.org.il/WarningMessages/alerts.json');
+	return mb_convert_encoding($JSON , 'UTF-8' , 'UTF-16');
+}
+
 // prints alarms of rocket attacks on Israel using php
 function CheckOrefData()
 {
 	$sOutFrmt = 'מרחב: %s, קוד מרחב: %s';
-	$JSON = file_get_contents('http://www.oref.org.il/WarningMessages/alerts.json');
-	$JSON = mb_convert_encoding($JSON , 'UTF-8' , 'UTF-16');
+
 
 	// Example output:
 	//$JSON = '{"id" : "1405853194651", "title" : "פיקוד העורף התרעה במרחב ", "data" : ["שפלה 175","שפלה 174", " עוטף עזה 666"]}';
 
 	while (true) {
-		$arr = json_decode($JSON,true);
+		$JSON = fetchJSON();
+		if (empty($JSON)) continue;
+
+		$arr = json_decode($JSON, true /* returned objects will be converted into associative arrays */);
+
 		if (!empty($arr['data'])) {
 
 			echo date('r') . "\n";
