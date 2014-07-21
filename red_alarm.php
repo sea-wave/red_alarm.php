b3ed7881d6ec07e3fbd64cc9e0218b96f2c2f0f3
Gal Gur-Arie <gal.ga75@gmail.com>
2014-07-21 23:20:30 +0300

    Fixing case of spaces in the area name

--- red_alarm.php
+++ red_alarm.php
@@ -1,4 +1,5 @@
 <?php
+set_time_limit(0);
 date_default_timezone_set('Asia/Jerusalem');
 
 // prints alarms of rocket attacks on Israel using php
@@ -8,6 +9,8 @@ function CheckOrefData()
 	$JSON = file_get_contents('http://www.oref.org.il/WarningMessages/alerts.json');
 	$JSON = mb_convert_encoding($JSON , 'UTF-8' , 'UTF-16');
 
+	// Example output:
+	//$JSON = '{"id" : "1405853194651", "title" : "פיקוד העורף התרעה במרחב ", "data" : ["שפלה 175","שפלה 174", " עוטף עזה 666"]}';
 
 	while (true) {
 		$arr = json_decode($JSON,true);
@@ -16,10 +19,10 @@ function CheckOrefData()
 			echo date('r') . "\n";
 
 			$iTotal = count($arr['data']);
-			for ($i=0;$i<$iTotal;$i++) {
-				preg_match('/(\p{Hebrew}+)?\s(.*)$/u', $arr['data'][$i], $arrMatches);
+			for ($i=0; $i<$iTotal; $i++) {
+				preg_match('/([\p{Hebrew}|\s]+).*?([\d]+)$/u', $arr['data'][$i], $arrMatches);
 				if (!empty($arrMatches) && (count($arrMatches) === 3)) { 
-					echo "\t" . sprintf($sOutFrmt ,$arrMatches[1],$arrMatches[2])."\n";
+					echo "\t" . sprintf($sOutFrmt ,trim($arrMatches[1]),$arrMatches[2])."\n";
 				}
 			}
 			echo "\n";
